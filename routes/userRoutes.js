798411e5caaad13f17cea345c894614335ae62e0
mrdevip15 <69381800+mrdevip15@users.noreply.github.com>
2025-07-28 19:07:28 +0800

    remove rate limit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,5 @@
 // routes/userRoutes.js
 const express = require('express');
-const rateLimit = require('express-rate-limit');
-const tokenLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // Limit each IP to 10 requests per windowMs for this route
-    message: 'Too many requests for token registration, please try again later.',
-  });
 const { dashboard, updateUserHandler, topremium, saveAnswers, registerToken, miniTo, addExamCompletedHandler, disqualifyUser, handbook, getPembahasan, changePassword } = require('../controllers/userController');
 const ensureAuthenticated = require('../middleware/authMiddleware');
 const navUser = require('../middleware/navUser');
@@ -17,7 +11,7 @@ router.get('/dashboard', ensureAuthenticated, dashboard);
 //   });
 router.get('/to-premium/:token',ensureAuthenticated, topremium);
 router.get('/dashboard/mini-to',ensureAuthenticated, miniTo);
-router.post('/to-premium/input-token',ensureAuthenticated, tokenLimiter, registerToken);
+router.post('/to-premium/input-token',ensureAuthenticated, registerToken);
 router.post('/updateUser',ensureAuthenticated, updateUserHandler);
 router.post('/save-answers',ensureAuthenticated, saveAnswers);
 router.post('/disqualify/:examId', ensureAuthenticated, disqualifyUser);
